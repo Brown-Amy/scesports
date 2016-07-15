751e7bd3418ca5ea403333e010051e236f8fff4a
Ammon Lockwood <ammon@plaidtie.net>
2016-07-15 16:53:24 -0600

    Fix: Fixes the edit_games function to correctly update the database

--- models/games_model.php
+++ models/games_model.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 function get_games() {
     global $db;
     $query = 'SELECT * FROM games
@@ -6,7 +6,7 @@ function get_games() {
     $statement = $db->prepare($query);
     $statement->execute();
     return $statement;
-} 
+}
 
 function add_game($name) {
     global $db;
@@ -22,15 +22,15 @@ function add_game($name) {
 function edit_game($id, $name) {
     global $db;
       $query = "UPDATE games
-                SET name = :name,
+                SET name = :name
                 WHERE id = :id";
-            
+
         $statement = $db->prepare($query);
         $statement->bindValue(':name', $name);
         $statement->bindValue(':id', $id);
         $statement->execute();
         $statement->closeCursor();
-    
+
 }
 function delete_game($id) {
     global $db;
