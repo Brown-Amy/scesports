b4cd0645d50de8438fcabb51bd985b4147d746d5
Brown-Amy <amybrown@Amys-MacBook-Pro.local>
2016-07-18 11:02:27 -0700

    parentlist.php
    changes to my parent list.php

--- views/volunteer/parentlist.php
+++ views/volunteer/parentlist.php
@@ -13,19 +13,21 @@
                 <th>Game</th>
                 <th>Mentor</th>
                 <th>Comments</th>
+                <th>&nbsp;</th>
+                <th>&nbsp;</th>
 
             </tr>
             <?php foreach ($parents as $parent) : ?>
             <tr>
                 <td class="first"><?php echo $parent['first_name']; ?></td>
-                <td><?php echo $parent['last_name']; ?></td>
+                <!--<td><?php// echo $parent['last_name']; ?></td>
 
-                <td><?php echo $parent['email']; ?></td>
-                <td><?php echo $parent['phone']; ?></td>
-                <td><?php echo $parent['child_name']; ?></td>
-                <td><?php echo $mentor_request; ?></td>
-                <td><?php echo $parent['game']; ?></td>
-                <td><?php echo $parent['comments']; ?></td>
+                <td><?php //echo $parent['email']; ?></td>
+                <td><?php //echo $parent['phone']; ?></td>
+                <td><?php //echo $parent['child_name']; ?></td>
+                <!--<td><?php //echo $mentor_request; ?></td>
+                <td><?php //echo $parent['game']; ?></td>
+                <td><?php //echo $parent['comments']; ?></td-->
                 
                  <td><form action="." method="post">
                           <input type="hidden" name="action"
