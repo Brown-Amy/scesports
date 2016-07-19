df2d0672b3de87eb59d653031e644e07e8f1becc
Ammon Lockwood <ammon@plaidtie.net>
2016-07-18 19:41:30 -0600

    Fix: fixes the query for the parents so that there is no conflicting 'id' field

--- views/volunteer/parentlist.php
+++ views/volunteer/parentlist.php
@@ -6,8 +6,9 @@
         <h1>parents</h1>
         <table id="parentlist">
             <tr>
-                <th class="first">First Name</th>
-                <th>Last Name</th>
+                <!-- <th class="first">First Name</th>
+                <th>Last Name</th> -->
+                <th class="first">Name</th>
                 <th>Email</th>
                 <th>Phone Number</th>
                 <th>Child Name</th>
@@ -20,24 +21,24 @@
             </tr>
             <?php foreach ($parents as $parent) : ?>
             <tr>
-                <td class="first"><?php echo $parent['first_name']; ?></td>
+                <td class="first"><?php echo $parent['full_name']; ?></td>
 
-                <td><?php echo $parent['last_name']; ?></td>
+                <!-- <td><?php echo $parent['last_name']; ?></td> -->
 
                 <td><?php echo $parent['email']; ?></td>
                 <td><?php echo $parent['phone']; ?></td>
                 <td><?php echo $parent['child_name']; ?></td>
                 <td><?php echo $parent['mentor'] == 1?'yes': 'no'; ?></td>
-                <td><?php echo $parent['name']; ?></td>
+                <td><?php echo $parent['game_name']; ?></td>
                 <td><?php echo $parent['comment']; ?></td>
-                
+
                  <td><form action="." method="post">
                           <input type="hidden" name="action"
                            value="show_edit_form">
-                
+
                     <input type="hidden" name="id"
                            value="<?php echo $parent['id']; ?>">
-                    
+
                     <input type="submit" value="Edit">
                 </form></td>
                 <td><form action="." method="post">
@@ -45,14 +46,14 @@
                            value="delete_parent">
                     <input type="hidden" name="id"
                            value="<?php echo $parent['id']; ?>">
-                  
+
                     <input type="submit" value="Delete">
                 </form></td>
             </tr>
             <?php endforeach; ?>
         </table>
         <p><a href="?action=add_parent_form">Add parent</a></p>
-              
+
     </section>
     </div>
 </main>
